c07a3ab35a0e5910741cab3d23cffbd471fc94c8
awadheshkumar <awadhesh.kumar@techment.com>
2019-08-11 15:09:35 +0530

    Added validations to country model.

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,5 @@
 class Country < ApplicationRecord
+    validates :name, presence: true,length: {minimum:3,maximum:45}
+    validates :short_name, presence: true, length: {minimum:2,maximum:45}
+    validates :code, presence: true, length: {minimum:2,maximum:45}
 end
