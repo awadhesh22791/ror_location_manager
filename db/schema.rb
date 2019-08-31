d879c8e26dcd6c7dcf8be474e23ef5bf4e88186c
awadheshkumar <awadhesh.kumar@techment.com>
2019-08-31 17:42:47 +0530

    [Enhancement] Removed password column.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_31_120117) do
+ActiveRecord::Schema.define(version: 2019_08_31_120958) do
 
   create_table "countries", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2019_08_31_120117) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "username"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
