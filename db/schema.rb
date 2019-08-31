1c9d337a8c36c1ae62b1689e46a810777ef2c226
awadheshkumar <awadhesh.kumar@techment.com>
2019-08-31 15:07:57 +0530

    [Enhancement] Added user model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_131132) do
+ActiveRecord::Schema.define(version: 2019_08_31_062104) do
 
   create_table "countries", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 2019_08_25_131132) do
     t.datetime "deleted_at"
     t.integer "country_id"
     t.index ["deleted_at"], name: "index_states_on_deleted_at"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.string "email"
+    t.index ["deleted_at"], name: "index_users_on_deleted_at"
   end
 
 end
