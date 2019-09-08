c828bf94b4071cb9cdba3b2b14e08ff5b4216514
awadheshkumar <awadhesh.kumar@techment.com>
2019-09-08 12:11:23 +0530

    [Enhancement] Added feature to add, delete, show, list and edit city.

---
+++ app/controllers/cities_controller.rb
@@ -0,0 +1,54 @@
+class CitiesController < ApplicationController
+    before_action :set_city,only:[:show,:edit,:destroy,:update]
+
+    def index
+        @cities=City.page params[:page]
+    end
+
+    def show
+    end
+
+    def new
+        @city=City.new
+    end
+
+    def create
+
+        @city=City.new(city_params)
+        if @city.save
+            flash[:success]="City was created successfully."
+            redirect_to city_path(@city)
+        else
+            render "new"
+        end
+
+    end
+
+    def update
+        if @city.update(city_params)
+            flash[:success]="City updated successfully."
+            redirect_to cities_path
+        else
+            render "edit"
+        end
+    end
+
+    def edit
+    end
+
+    def destroy
+        @city.destroy
+        flash[:danger]="City deleted successfully."
+        redirect_to cities_path
+    end
+
+    private
+        def set_city
+            @city=City.find(params[:id])
+        end
+
+        def city_params
+            params.require(:city).permit(:name)
+        end
+
+end
